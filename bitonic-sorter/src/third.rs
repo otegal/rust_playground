cc39bbcccf7377083633a7f072ea9013e05c301c
hhiroki <gyouzakuuka@gmail.com>
2020-02-08 20:21:19 +0900

    まずはテストコード

---
+++ bitonic-sorter/src/third.rs
@@ -0,0 +1,59 @@
+#[cfg(test)]
+mod tests {
+    struct Student {
+        first_name: String,
+        last_name: String,
+        age: u8,
+    }
+
+    impl Student {
+        fn new(first_name: &str, last_name: &str, age: u8) -> Self {
+            Self {
+                first_name: first_name.to_string(),
+                last_name: last_name.to_string(),
+                age,
+            }
+        }
+    }
+
+    // 親モジュールのsort関数を使用する
+    use super::{is_power_of_two, sort, sort_by};
+    use crate::SortOrder::*;
+
+    #[test]
+    fn sort_students_by_age_ascending() {
+        let taro = Student::new("Taro", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("Kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+        let expected = vec![&hanako, &kyoko, &taro, &ryosuke];
+
+        assert_eq!(
+            sort_by(&mut x, &|a, b| a.age.cmp(&b.age)),
+            Ok(())
+        );
+        assert_eq!(x, expected)
+    }
+
+    #[test]
+    fn sort_students_by_name_ascending() {
+        let taro = Student::new("Taro", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("Kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+        let expected = vec![&ryosuke, &kyoko, &hanako, &taro];
+
+        assert_eq!(
+            sort_by(
+                &mut x,
+                &|a, b| a.last_name.cmp(&b.last_name).then_with(|| a.first_name.cmp(&b.last_name))
+            ),
+            Ok(())
+        );
+        assert_eq!(x, expected)
+    }
+}
