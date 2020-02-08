93ce4ffa22fb373208154601eec80be7db4f8089
hhiroki <gyouzakuuka@gmail.com>
2020-02-08 19:23:27 +0900

    まずはテストコードを書く

---
+++ bitonic-sorter/src/second.rs
@@ -0,0 +1,62 @@
+// pub と修飾子をつけることで他モジュールからも呼び出せるパブリックな関数にする。
+pub fn sort(x: &mut [u32], up: bool) {
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);  // xのスライスの最初からmid_pointまで
+        sort(&mut x[mid_point..], false); // xのスライスのmidpointから最後まで
+        sub_sort(x, up);
+    }
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            x.swap(i, mid_point + i);
+        }
+    }
+}
+
+// cargo testを実行した時のみコンパイルされる
+#[cfg(test)]
+mod tests {
+    // 親モジュールのsort関数を使用する
+    use super::sort;
+
+    // #[test]のアノテーションが付いた関数はcargo testした時に実行される
+    #[test]
+    fn sort_u32_ascending() {
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, true);
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+
+    #[test]
+    fn sort_u32_descending() {
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, false);
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    #[test]
+    fn sort_str_ascending() {
+        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
+        sort(&mut x, true)
+        assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
+    }
+
+    fn sort_str_descending() {
+        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
+        sort(&mut x, false)
+        assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"]);
+    }
+}
