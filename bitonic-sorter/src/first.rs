b8fe5ff10133c66bed9b7bbb7661d9df6b3ff6e2
hhiroki <gyouzakuuka@gmail.com>
2020-02-08 18:44:18 +0900

    雛形作成

---
+++ bitonic-sorter/src/first.rs
@@ -0,0 +1,12 @@
+// pub と修飾子をつけることで他モジュールからも呼び出せるパブリックな関数にする。
+pub fn sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
