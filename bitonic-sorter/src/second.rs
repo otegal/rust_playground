7f47653cf52717b67e20037bab46268e6bcaba01
hhiroki <gyouzakuuka@gmail.com>
2020-02-08 19:41:36 +0900

    asc, descをenumで定義した

--- bitonic-sorter/src/second.rs
+++ bitonic-sorter/src/second.rs
@@ -1,9 +1,18 @@
+use super::SortOrder;
+
 // pub と修飾子をつけることで他モジュールからも呼び出せるパブリックな関数にする。
-pub fn sort<T: Ord>(x: &mut [T], up: bool) {
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) {
+    match *order {
+        SortOrder::Ascending => do_sort(x, true),
+        SortOrder::Descending => do_sort(x, false),
+    }
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);  // xのスライスの最初からmid_pointまで
-        sort(&mut x[mid_point..], false); // xのスライスのmidpointから最後まで
+        do_sort(&mut x[..mid_point], true);  // xのスライスの最初からmid_pointまで
+        do_sort(&mut x[mid_point..], false); // xのスライスのmidpointから最後まで
         sub_sort(x, up);
     }
 }
@@ -31,33 +40,34 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 mod tests {
     // 親モジュールのsort関数を使用する
     use super::sort;
+    use crate::SortOrder::*;
 
     // #[test]のアノテーションが付いた関数はcargo testした時に実行される
     #[test]
     fn sort_u32_ascending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
     #[test]
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, false);
+        sort(&mut x, &Descending);
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
 
     #[test]
     fn sort_str_ascending() {
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
         assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
     }
 
     #[test]
     fn sort_str_descending() {
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, false);
+        sort(&mut x, &Descending);
         assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"]);
     }
 }
