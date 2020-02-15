e340b8aba570690c5dba09ec328f315d6724190b
hhiroki <gyouzakuuka@gmail.com>
2020-02-15 21:00:36 +0900

    並行処理させる

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -28,7 +28,13 @@ fn main() {
         Philosopher::new("kin"),
     ];
 
-    for p in philosophers {
-        p.eat();
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        thread::spawn(move || {
+            p.eat();
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
     }
 }
