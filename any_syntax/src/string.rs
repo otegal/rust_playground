8bd661caa7661956d1624d0e049784abf95b6608
hhiroki <gyouzakuuka@gmail.com>
2020-05-06 23:30:37 +0900

    some string syntax

--- any_syntax/src/string.rs
+++ any_syntax/src/string.rs
@@ -9,6 +9,35 @@ pub fn string_practice() {
     println!("ch is {}", ch);
 
     test();
+
+    let s = "hello rust world.";
+    println!("s is {}", s);
+
+    let hello = &s[0..5];
+    let world = &s[11..];
+    println!("{} {}", hello, world);
+
+    let len = s.len();
+    println!("len is {}", len);
+
+    let mut s = String::new();
+    s.push_str("hello ");
+    s.push_str("rust ");
+    s.push_str("world.");
+    println!("s is {}", s);
+
+    let hello = "HELLO ";
+    let rust = "RUST ";
+    let world = "WORLD.";
+    let s = format!("{}{}{}", hello, rust, world);
+    println!("s is {}", s);
+
+    // &str型 　 後から変更出来ない文字列
+    // &String型 変更出来る文字列
+    let s = "hello rust world.".to_string();
+    println!("s is {}", s);
+    let s = String::from("hello rust world.");
+    println!("s is {}", s);
 }
 
 fn test() {
