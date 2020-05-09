7db2a489b491387a90945e9c9aa8976da275d3cb
hhiroki <gyouzakuuka@gmail.com>
2020-05-09 12:32:35 +0900

    control

---
+++ any_syntax/src/control.rs
@@ -0,0 +1,8 @@
+pub fn control_practice() {
+    let v = vec![10, 20, 30, 40, 50];
+    print!("v is ");
+    for i in &v {
+        print!("{} ", i);
+    }
+    println!("");
+}
--- any_syntax/src/main.rs
+++ any_syntax/src/main.rs
@@ -1,8 +1,13 @@
 #[allow(dead_code)]
 mod types;
+
+#[allow(dead_code)]
 mod string;
+
+mod control;
 
 fn main() {
     // types::types_practice();
-    string::string_practice();
+    // string::string_practice();
+    control::control_practice();
 }
