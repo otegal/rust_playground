b2ee798bb5c29001ac745fab00424d176dbcc033
hhiroki <gyouzakuuka@gmail.com>
2020-05-09 13:31:05 +0900

    pattern match

--- any_syntax/src/control.rs
+++ any_syntax/src/control.rs
@@ -1,3 +1,11 @@
+#[derive(Debug)]
+enum LANG {
+    JAPANESE = 81,
+    ENGLISH = 44,
+    CHINESE = 86,
+    FRANCH = 33,
+}
+
 pub fn control_practice() {
     let v = vec![10, 20, 30, 40, 50];
     print!("v is ");
@@ -17,4 +25,17 @@ pub fn control_practice() {
         print!("{}:{} ", index, value);
     }
     println!("");
+
+    let lang = LANG::JAPANESE;
+    println!("lang is {}", lang as i32);
+
+    let lang = LANG::JAPANESE;
+    let m = match lang {
+        LANG::JAPANESE => "日本語",
+        LANG::ENGLISH => "英語",
+        LANG::CHINESE => "中国語",
+        LANG::FRANCH => "フランス語",
+        _ => "未定義"
+    };
+    println!("lang is {}", m);
 }
