f0397579cb353861731c12dfee805687af95ff3b
hhiroki <gyouzakuuka@gmail.com>
2020-05-23 19:49:25 +0900

    result match

--- web_api/src/main.rs
+++ web_api/src/main.rs
@@ -1,23 +1,29 @@
-use reqwest::StatusCode;
+// use reqwest::StatusCode;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let page = 7;
     let url = format!("http://openccpm.com/unknown.txt?p={}", page);
     println!("call {}", &url);
-    let res = reqwest::get(&url).await?;
-    match res.status() {
-        StatusCode::OK => {
-            let body = res.text().await?;
-            println!("response is \n{}", body);
-        },
-        StatusCode::NOT_FOUND => {
-            println!("error: 目的のページがありませんでした。");
-        },
-        _ => {
-            println!("error: その他のエラーが発生しました。");
-        },
-    }
+    // let res = reqwest::get(&url).await?;
+    // match res.status() {
+    //     StatusCode::OK => {
+    //         let body = res.text().await?;
+    //         println!("response is \n{}", body);
+    //     },
+    //     StatusCode::NOT_FOUND => {
+    //         println!("error: 目的のページがありませんでした。");
+    //     },
+    //     _ => {
+    //         println!("error: その他のエラーが発生しました。");
+    //     },
+    // }
 
+    if let Ok(res) = reqwest::get(&url).await {
+        let body = res.text().await?;
+        println!("response is \n {}", body);
+    } else {
+        println!("error: Not found web server.");
+    }
     Ok(())
 }
