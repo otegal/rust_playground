967cec3f27c3bf725dcecaefa0d0d1f4fba96025
hhiroki <gyouzakuuka@gmail.com>
2020-05-23 19:11:17 +0900

    debug format implement

--- any_syntax/src/macros.rs
+++ any_syntax/src/macros.rs
@@ -1,9 +1,15 @@
-#[derive(Debug)]
+// #[derive(Debug)]
 struct Person {
     id: i32,
     name: String,
     age: i32,
     addr: String,
+}
+
+impl std::fmt::Debug for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{ {}: `{}` in {} }}", self.id, self.name, self.addr)
+    }
 }
 
 
