1f1091327c0dc10af6ac7b6ad159c35ad24d2e13
hhiroki <gyouzakuuka@gmail.com>
2021-05-09 20:24:19 +0900

    learning

--- embedded_playground/hello/src/main.rs
+++ embedded_playground/hello/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("Hello, {}!", "world"); // {}を使う場合、std::fmt::Displayトレイトを実装している場合のみ。
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("a = {:?}", a);  //  {:?}を使うと表示できる場合もある
+    // println!("a = {}", a); // こっちだとコンパイルエラーになる
 }
