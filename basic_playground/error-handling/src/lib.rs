807b32245c96aa7fbdd7f8293171b686c3609ec7
hhiroki <gyouzakuuka@gmail.com>
2021-09-24 23:54:40 +0900

    Resultを受け取ってテストする

--- basic_playground/error-handling/src/lib.rs
+++ basic_playground/error-handling/src/lib.rs
@@ -14,3 +14,27 @@ fn test_file_open() {
     assert_eq!(file_open("s.txt"), "error");
     assert_eq!(file_open("src/lib.rs"), "ok");
 }
+
+fn file_open_return_result_type(path: &str) -> std::io::Result<String> {
+    fs::read_to_string(path)
+}
+
+#[test]
+fn test_file_open_return_result_type() {
+    let not_exist_file_path = "s.txt";
+
+    let fail_result = file_open_return_result_type(not_exist_file_path);
+    assert_eq!(fail_result.is_err(), true);
+    assert_eq!(fail_result.is_ok(), false);
+
+    let unwrap = match file_open_return_result_type(not_exist_file_path) {
+        Ok(_s) => "ok",
+        Err(_e) => "err",
+    };
+    assert_eq!(unwrap, "err");
+
+    let method_chain_unwrap = file_open_return_result_type(not_exist_file_path)
+        .map(|_s| 0)
+        .unwrap_or_else(|_err| 1);
+    assert_eq!(method_chain_unwrap, 1);
+}
